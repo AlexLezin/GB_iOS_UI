c0af1a472241f5d58cc7505cf340836f4333cbd7
Александр Лезин <aleksandr.lyozin@gmail.com>
2019-05-14 21:19:27 +0300

    init
    Init commit (single-view app)

---
+++ GB_iOS_UI_Course/GB_iOS_UI_Course/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GB_iOS_UI_Course
+//
+//  Created by Alexander Lezin on 14/05/2019.
+//  Copyright © 2019 Alexander Lezin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
