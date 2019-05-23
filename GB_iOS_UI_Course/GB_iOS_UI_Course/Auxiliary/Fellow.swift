b2bb0ea3b4291276668a23998ffcc7653e445357
Александр Лезин <aleksandr.lyozin@gmail.com>
2019-05-23 23:58:21 +0300

    Homework 3 is done
    Some features are still need to be done properly

---
+++ GB_iOS_UI_Course/GB_iOS_UI_Course/Auxiliary/Fellow.swift
@@ -0,0 +1,18 @@
+//
+//  Fellow.swift
+//  GB_iOS_UI_Course
+//
+//  Created by Alexander Lezin on 23/05/2019.
+//  Copyright © 2019 Alexander Lezin. All rights reserved.
+//
+
+import UIKit
+
+struct Fellow {
+    var name: String
+    var avatar: UIImage
+}
+
+let steveJobs = Fellow(name: "Steeve Jobs", avatar: #imageLiteral(resourceName: "Image"))
+let johnIve = Fellow(name: "Johnathan Ive", avatar: #imageLiteral(resourceName: "Image-2"))
+let timCook = Fellow(name: "Tim Cook", avatar: #imageLiteral(resourceName: "Image-1"))
