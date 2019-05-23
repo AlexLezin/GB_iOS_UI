e9b81157fed22ddab6e85ead3b43ebcbce7fa459
Александр Лезин <aleksandr.lyozin@gmail.com>
2019-05-23 21:51:21 +0300

    Classes added
    Classes files for Classes section was added

---
+++ GB_iOS_UI_Course/GB_iOS_UI_Course/Classes/All Classes/AllCitiesTableViewCell.swift
@@ -0,0 +1,25 @@
+//
+//  AllCitiesTableViewCell.swift
+//  GB_iOS_UI_Course
+//
+//  Created by Alexander Lezin on 23/05/2019.
+//  Copyright © 2019 Alexander Lezin. All rights reserved.
+//
+
+import UIKit
+
+class AllCitiesTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var classTitle: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
